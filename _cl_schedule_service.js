3a8285354e05d365c4ad512c8de99ca7ecc8780c
Anesu <anesu4@yahoo.com.au>
2020-12-24 09:47:57 +0800

    Anesu - Scheduled Service

---
+++ _cl_schedule_service.js
@@ -0,0 +1,30 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ */
+
+/** Module Description
+ * 
+ * NSVersion    Date                        Author         
+ * 2.00         2020-10-22 09:33:08         Anesu
+ *
+ * Description: 
+ * 
+ * @Last Modified by:   Anesu
+ * @Last Modified time: 2020-11-217 16:49:26
+ * 
+ */
+
+define(['N/email', 'N/runtime', 'N/search', 'N/record', 'N/http', 'N/log', 'N/error', 'N/url', 'N/format', 'N/currentRecord'],
+    function(email, runtime, search, record, http, log, error, url, format, currentRecord) {
+        
+        
+        function pageInit(){
+
+        } 
+        
+
+        return {
+            pageInit: pageInit
+        }
+});
