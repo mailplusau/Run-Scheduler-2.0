ba2316e82beba30a7cea4ab6667ae6c9935207a9
Sruti Desai <sruti.desai@student.unsw.edu.au>
2021-01-20 09:54:15 +1100

    csv formatting

--- mp_cl_import_excel.js
+++ mp_cl_import_excel.js
@@ -21,15 +21,122 @@ function(error, runtime, search, url, record, format, email, currentRecord ) {
          */
         function pageInit() {
         
+            $(document).on('change', '.zee_dropdown', function(event) {
+                var zee = $(this).val();
+                var zee_text = $(this).text();
+                var currentScript = currentRecord.get();
+                currentScript.setValue({
+                    fieldId: 'zee',
+                    value: zee
+                });              
+
+                createCSV(zee);
+                // var url = baseURL + "/app/site/hosting/scriptlet.nl?script=1151&deploy=1";
+                // url += "&zee=" + zee + "";
+                // window.location.href = url;
+            });
+
         }
+
         function saveRecord(context) {
 
             return true;
         }
 
-        function onclick_importButton() {
-            alert('worked');
+        function onclick_downloadButton() {
+            var currentScript = currentRecord.get();
+            var zee = currentScript.getValue({
+                fieldId: 'zee',
+            });
+
+            var zeeRec = record.load({
+                type: record.Type.PARTNER,
+                id: zee,
+            });
+            
+            var zee_text = zeeRec.getValue({
+                fieldId: 'entityid'
+            })
+
+            if (zee == 0 && role != 1000) {
+                alert('Please Select a Zee before downloading a template');
+            } else {
+                downloadCsv(zee_text);
+
+                alert('worked');
+            }  
         }
+
+        /**
+         * Create the CSV and store it in the hidden field 'custpage_table_csv' as a string.
+         */
+        function createCSV(zeeVal) {
+            var headers = ["Customer ID", "Customer Name", "Service ID", "Service Name", "Price", "Frequency", "PO Box# or DX#", "Stop 1 Location", "Stop 1 Duration", "Stop 1 Time", "Notes", "Stop 2 Location", "Stop 2 Duration", "Stop 2 Time", "Notes", "Driver Name"]
+            headers = headers.slice(0, headers.length); // .join(', ')
+
+            var csv = headers + "\n";
+            
+            var serviceSearch = search.load({
+                id: 'customsearch_rp_services',
+                type: 'customrecord_service'
+            });
+
+            var defaultFilters = serviceSearch.filters;
+            defaultFilters.push(search.createFilter({
+                name: 'custrecord_service_franchisee',
+                operator: search.Operator.ANYOF,
+                values: zeeVal
+            }));
+
+
+            serviceSearch.filters = defaultFilters;
+            var resultSetCustomer = serviceSearch.run();
+            
+            resultSetCustomer.each(function(searchResult) {
+                var custid = searchResult.getValue({ name: "custrecord_service_customer", join: null, summary: search.Summary.GROUP});
+                var companyname = searchResult.getValue({ name: "companyname", join: "CUSTRECORD_SERVICE_CUSTOMER", summary: search.Summary.GROUP});
+                var service_id = searchResult.getValue({ name: "internalid", join: null, summary: search.Summary.GROUP});
+                var service_name = searchResult.getText({ name: "custrecord_service", join: null, summary: search.Summary.GROUP});
+                var service_price = searchResult.getValue({ name: "custrecord_service_price", join: null, summary: search.Summary.GROUP});
+                
+                var row = new Array();
+                row[0] = custid; row[1]= companyname; row[2] = service_id; row[3] = service_name; row[4] = service_price;
+                csv += row.join(',');
+                csv += "\n";
+                return true;
+            });
+
+            var val1 = currentRecord.get();
+            val1.setValue({
+                fieldId: 'custpage_table_csv',
+                value: csv
+            });
+
+
+            return true;
+        }
+
+
+        function downloadCsv(zeeVal) {
+            var val1 = currentRecord.get();
+            var csv = val1.getValue({
+                fieldId: 'custpage_table_csv',
+            });
+            var a = document.createElement("a");
+            document.body.appendChild(a);
+            a.style = "display: none";
+            var content_type = 'text/csv';
+            var csvFile = new Blob([csv], { type: content_type });
+            var url = window.URL.createObjectURL(csvFile);
+            var filename = zeeVal + '_template.csv';
+            a.href = url;
+            a.download = filename;
+            a.click();
+            window.URL.revokeObjectURL(url);
+
+
+        }
+
         function isNullorEmpty(strVal) {
             return (strVal == null || strVal == '' || strVal == 'null' || strVal == undefined || strVal == 'undefined' || strVal == '- None -');
         }
@@ -37,7 +144,7 @@ function(error, runtime, search, url, record, format, email, currentRecord ) {
         return {
             pageInit: pageInit,
             saveRecord: saveRecord,
-            onclick_importButton: onclick_importButton
+            onclick_downloadButton: onclick_downloadButton,
             
         };  
     }
--- mp_sl_import_excel.js
+++ mp_sl_import_excel.js
@@ -19,30 +19,81 @@ function(ui, email, runtime, search, record, http, log, redirect, format, file, 
     var role = runtime.getCurrentUser().role;
     if (role == 1000) {
         //Franchisee
-        zee = runtime.getCurrentUser();
+        zee = runtime.getCurrentUser().id;
     } 
 
     function onRequest(context) {  
         
         if (context.request.method === 'GET') {
+            var inlineHtml = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"><script src="//code.jquery.com/jquery-1.11.0.min.js"></script><link type="text/css" rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css"><link href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"><script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script><link rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2060796&c=1048144&h=9ee6accfd476c9cae718&_xt=.css"/><script src="https://1048144.app.netsuite.com/core/media/media.nl?id=2060797&c=1048144&h=ef2cda20731d146b5e98&_xt=.js"></script><link type="text/css" rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2090583&c=1048144&h=a0ef6ac4e28f91203dfe&_xt=.css">';
+            // Load DataTables
+            inlineHtml += '<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">';
+            inlineHtml += '<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>';
+            inlineHtml += '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">';
+            inlineHtml += '<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>';
+            inlineHtml += '<div></div>';    
+
             var form = ui.createForm({
                 title: 'Import Excel'
             });
             form.addButton({
                 id : 'import',
-                label : 'IMPORT EXCEL',
-                functionName : 'onclick_importButton()'
+                label : 'DOWNLOAD TEMPLATE',
+                functionName : 'onclick_downloadButton()'
             });
-            form.addField({
-                id: 'upload_rs_csv',
-                label: 'IMPORT EXCEL',
-                type: ui.FieldType.FILE
-            });
-
             
             form.addSubmitButton({
                 label: 'Submit'
             });
+
+            if (role != 1000) {
+                inlineHtml += franchiseeDropdownSection();
+            }
+
+            
+            form.addField({
+                id: 'zee',
+                type: ui.FieldType.TEXT,
+                label: 'zee'
+            }).updateDisplayType({
+                displayType: ui.FieldDisplayType.HIDDEN
+            }).defaultValue = zee;
+
+            form.addField({
+                id: 'zee_text',
+                type: ui.FieldType.TEXT,
+                label: 'zee_text'
+            }).updateDisplayType({
+                displayType: ui.FieldDisplayType.HIDDEN
+            }).defaultValue = zee;
+
+            
+
+            form.addField({
+                id: 'custpage_table_csv',
+                type: ui.FieldType.TEXT,
+                label: 'Table CSV'
+            }).updateDisplayType({
+                displayType: ui.FieldDisplayType.HIDDEN
+            });
+
+            form.addField({
+                id: 'preview_table',
+                type: ui.FieldType.INLINEHTML,
+                label: 'preview_table'
+            }).updateLayoutType({
+                layoutType: ui.FieldLayoutType.OUTSIDEBELOW
+            }).updateBreakType({
+                breakType: ui.FieldBreakType.STARTROW
+            }).defaultValue = inlineHtml;
+
+            form.addField({
+                id: 'upload_rs_csv',
+                label: 'IMPORT EXCEL',
+                type: ui.FieldType.FILE
+            }); 
+
+            
             form.clientScriptFileId = 4620348; //PROD = 4620348, SB = ??
 
             context.response.writePage(form);
@@ -97,6 +148,7 @@ function(ui, email, runtime, search, record, http, log, redirect, format, file, 
                 var notes = rs_values[10];
                 var driver = rs_values[11];
 
+
                 log.debug({
                     title: 'comp',
                     details: companyName
@@ -115,6 +167,49 @@ function(ui, email, runtime, search, record, http, log, redirect, format, file, 
         return (strVal == null || strVal == '' || strVal == 'null' || strVal == undefined || strVal == 'undefined' || strVal == '- None -');
     }
     
+
+    /**
+     * The Franchisee dropdown list.
+     * @param   {Number}    zee_id
+     * @return  {String}    `inlineQty`
+     */
+    function franchiseeDropdownSection() {
+        var inlineQty = '<div class="form-group container zee_dropdown_section >';
+
+        inlineQty += '<div class="row">';
+        // Franchisee dropdown field
+        inlineQty += '<div class="col-xs-12 zee_dropdown_div">';
+        inlineQty += '<div class="input-group col-xs-12">';
+        inlineQty += '<span class="input-group-addon" id="zee_dropdown_text">FRANCHISEE</span>';
+        inlineQty += '<select id="zee_dropdown" class="form-control zee_dropdown" required>';
+        inlineQty += '<option></option>';
+
+        // Load the franchisees options
+        var zeesSearch = search.load({
+            id: 'customsearch_job_inv_process_zee',
+            type: search.Type.PARTNER
+        });
+
+        var zeesSearchResults = zeesSearch.run();
+        zeesSearchResults.each(function (zeesSearchResult) {
+            var opt_zee_id = zeesSearchResult.getValue('internalid');
+                var opt_zee_name = zeesSearchResult.getValue('companyname');
+                
+            var selected_option = '';
+            if (role == 1000) {
+                zee_id = runtime.getCurrentUser().id; //Get Franchisee ID-- REMOVE TO TEST
+                selected_option = (opt_zee_id == zee_id) ? 'selected' : '';
+            }
+            inlineQty += '<option value="' + opt_zee_id + '" ' + selected_option + '>' + opt_zee_name + '</option>';
+            return true;
+        });
+
+        inlineQty += '</select>';
+        inlineQty += '</div></div></div></div>';
+
+        return inlineQty;
+    }
+
     function getDate() {
         var date = (new Date());
         // if (date.getHours() > 6) {
